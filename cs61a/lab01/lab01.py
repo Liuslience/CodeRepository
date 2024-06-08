a64d5b5b4a817b5b7a3e15746d75e57e1af770ad
Liuslience <38791402+Liuslience@users.noreply.github.com>
2024-06-09 00:18:28 +0800

    cs61a

---
+++ cs61a/lab01/lab01.py
@@ -0,0 +1,106 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if k == 0:
+        return 1
+
+    result = n
+    k -= 1
+    while k > 0:
+        n -= 1
+        result = result * n
+        k -= 1
+
+    return result
+
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    nums = []
+    count = 0
+    if n < k:
+        return count
+
+    while n > 0:
+        nums.append(n)
+        n -= k
+        count += 1
+
+    nums = nums[::-1]
+    for i in range(count):
+        print(nums[i])
+
+    return count
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    return sum(list(int(i) for i in str(y)))
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    l = list(int(i) for i in str(n))
+    stack = []
+    for i in range(len(l)):
+        stack.append(l[i])
+        if len(stack) >= 2:
+            if stack[-2] == 8 and stack[-1] == 8:
+                return True
+
+    return False
